51e56459378c4a2ff21199719c345d01051a6105
dap-13 <andy73594@gmail.com>
2025-04-22 12:04:13 +0300

    admin page

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,18 @@
+// src/auth/auth.module.ts
+
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { User, UserSchema } from './schemas/user.schema';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema, collection: 'data-users' },
+    ]),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule {}
---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,18 @@
+// src/auth/auth.service.ts
+
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  async validateAdmin(login: string, password: string): Promise<boolean> {
+    const user = await this.userModel.findOne({ login, password, role: 'admin' });
+    return !!user;
+  }
+}
+
+
