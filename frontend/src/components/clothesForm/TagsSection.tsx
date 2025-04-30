62163bb890e861b360c882e141f6542096b5e0d5
dap-13 <andy73594@gmail.com>
2025-04-30 13:26:51 +0300

    create clotheform

--- frontend/src/components/clothesForm/TagsSection.tsx
+++ frontend/src/components/clothesForm/TagsSection.tsx
@@ -1,46 +1,46 @@
-import { FormData } from "./ClothesForm";
-import styles from './ClothesForm.module.css';
+// import { FormData } from "./ClothesForm";
+// import styles from './ClothesForm.module.css';
 
-interface TagsSectionProps {
-  formData: FormData;
-  handleArrayChange: (
-    arrayName: 'careInstructions' | 'details' | 'tags',
-    index: number,
-    field: 'en' | 'uk' | '',
-    value: string
-  ) => void;
-  removeArrayItem: (arrayName: 'careInstructions' | 'details' | 'tags', index: number) => void;
-  addArrayItem: (arrayName: 'careInstructions' | 'details' | 'tags') => void;
-}
+// interface TagsSectionProps {
+//   formData: FormData;
+//   handleArrayChange: (
+//     arrayName: 'careInstructions' | 'details' | 'tags',
+//     index: number,
+//     field: 'en' | 'uk' | '',
+//     value: string
+//   ) => void;
+//   removeArrayItem: (arrayName: 'careInstructions' | 'details' | 'tags', index: number) => void;
+//   addArrayItem: (arrayName: 'careInstructions' | 'details' | 'tags') => void;
+// }
 
-export default function TagsSection({ 
-  formData, 
-  handleArrayChange, 
-  removeArrayItem, 
-  addArrayItem 
-}: TagsSectionProps) {
-  return (
-    <div className={styles.formSection}>
-      <h3>Tags</h3>
-      {formData.tags.map((tag, index) => (
-        <div key={index} className={styles.formGroup}>
-          <label>Tag {index + 1}:</label>
-          <input
-            type="text"
-            value={tag}
-            onChange={(e) => handleArrayChange('tags', index, '', e.target.value)}
-            required
-          />
-          {formData.tags.length > 1 && (
-            <button className={styles.formButton} type="button" onClick={() => removeArrayItem('tags', index)}>
-              Remove
-            </button>
-          )}
-        </div>
-      ))}
-      <button className={`${styles.formButton} ${styles.submitButton}`} type="button" onClick={() => addArrayItem('tags')}>
-        Add Tag
-      </button>
-    </div>
-  );
-}
+// export default function TagsSection({ 
+//   formData, 
+//   handleArrayChange, 
+//   removeArrayItem, 
+//   addArrayItem 
+// }: TagsSectionProps) {
+//   return (
+//     <div className={styles.formSection}>
+//       <h3>Tags</h3>
+//       {formData.tags.map((tag, index) => (
+//         <div key={index} className={styles.formGroup}>
+//           <label>Tag {index + 1}:</label>
+//           <input
+//             type="text"
+//             value={tag}
+//             onChange={(e) => handleArrayChange('tags', index, '', e.target.value)}
+//             required
+//           />
+//           {formData.tags.length > 1 && (
+//             <button className={styles.formButton} type="button" onClick={() => removeArrayItem('tags', index)}>
+//               Remove
+//             </button>
+//           )}
+//         </div>
+//       ))}
+//       <button className={`${styles.formButton} ${styles.submitButton}`} type="button" onClick={() => addArrayItem('tags')}>
+//         Add Tag
+//       </button>
+//     </div>
+//   );
+// }
