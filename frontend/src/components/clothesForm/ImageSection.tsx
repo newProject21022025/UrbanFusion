09fabc9664125fac093784a39ee5aae74182d31a
dap-13 <andy73594@gmail.com>
2025-05-09 13:07:01 +0300

    create card clothes

--- frontend/src/components/clothesForm/ImageSection.tsx
+++ frontend/src/components/clothesForm/ImageSection.tsx
@@ -66,7 +66,7 @@ export default function ImageSection({ formData, handleChange, setFormData }: Im
     <div className={styles.formSection}>
       <h3>{t('mainImage')}</h3>
 
-      <div className={styles.formGroup}>
+      {/* <div className={styles.formGroup}>
         <label>{t('uploadImage')}:</label>
         <input
           type="file"
@@ -80,9 +80,9 @@ export default function ImageSection({ formData, handleChange, setFormData }: Im
             <div style={{ width: `${uploadProgress}%` }}></div>
           </div>
         )}
-      </div>
+      </div> */}
 
-      {/* <div className={styles.customFileInput}>
+      <div className={styles.customFileInput}>
   <button type="button" onClick={() => fileInputRef.current?.click()} disabled={isUploading}>
     {t('chooseFile')}
   </button>
@@ -97,7 +97,7 @@ export default function ImageSection({ formData, handleChange, setFormData }: Im
     disabled={isUploading}
     style={{ display: 'none' }}
   />
-</div> */}
+</div>
 
       <div className={styles.formGroup}>
         <label>{t('imageUrl')}:</label>
