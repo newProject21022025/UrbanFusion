0bbb9a91ae7bc2b139944a4d7637ccc706495446
dap-13 <andy73594@gmail.com>
2025-05-20 13:04:44 +0300

    create registration

--- frontend/src/redux/slices/userSlice.ts
+++ frontend/src/redux/slices/userSlice.ts
@@ -2,22 +2,29 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface UserState {
-  userName: string | null;
   userId: string | null;
-  email: string | null;
   firstName: string | null;
   lastName: string | null;
-  // додай інші поля, які потрібні
+  email: string | null; // Це login
+  phone: string | null;
+  address: string | null;
+  postOfficeDetails: string | null;
+  dateOfBirth: string | null;
+  role: string | null;
   isLoggedIn: boolean;
 }
 
 const initialState: UserState = {
   userId: null,
-  email: null,
   firstName: null,
   lastName: null,
+  email: null,
+  phone: null,
+  address: null,
+  postOfficeDetails: null,
+  dateOfBirth: null,
+  role: null,
   isLoggedIn: false,
-  userName: null
 };
 
 const userSlice = createSlice({
@@ -28,23 +35,37 @@ const userSlice = createSlice({
       state,
       action: PayloadAction<{
         userId: string;
-        email: string;
         firstName: string;
         lastName: string;
+        email: string;
+        phone?: string;
+        address?: string;
+        postOfficeDetails?: string;
+        dateOfBirth?: string;
+        role?: string;
       }>
     ) => {
       state.userId = action.payload.userId;
-      state.email = action.payload.email;
       state.firstName = action.payload.firstName;
       state.lastName = action.payload.lastName;
-      state.userName = `${action.payload.firstName} ${action.payload.lastName}`;
+      state.email = action.payload.email;
+      state.phone = action.payload.phone || null;
+      state.address = action.payload.address || null;
+      state.postOfficeDetails = action.payload.postOfficeDetails || null;
+      state.dateOfBirth = action.payload.dateOfBirth || null;
+      state.role = action.payload.role || null;
       state.isLoggedIn = true;
     },
     clearUser: (state) => {
       state.userId = null;
-      state.email = null;
       state.firstName = null;
       state.lastName = null;
+      state.email = null;
+      state.phone = null;
+      state.address = null;
+      state.postOfficeDetails = null;
+      state.dateOfBirth = null;
+      state.role = null;
       state.isLoggedIn = false;
     },
   },
