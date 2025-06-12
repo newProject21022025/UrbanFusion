7666b0a20f06173005a5c6e8c35a2fa286405e16
dap-13 <andy73594@gmail.com>
2025-06-12 11:50:39 +0300

    Pagination callback

--- backend/src/callback/callback.controller.ts
+++ backend/src/callback/callback.controller.ts
@@ -1,39 +1,53 @@
 // src/callback/callback.controller.ts
-import { Controller, Get, Post, Body, Param, Put, Delete, HttpException, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Put,
+  Delete,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { CallbackService } from './callback.service';
 import { CreateCallbackDto } from './dto/create-callback.dto';
+import { Query } from '@nestjs/common';
+import { PaginationQueryDto } from '../callback/dto/pagination-query.dto';
 
 @Controller('callback')
 export class CallbackController {
   constructor(private readonly callbackService: CallbackService) {}
 
   @Post()
-  async create(@Body() createCallbackDto: CreateCallbackDto) {   
-      return await this.callbackService.create(createCallbackDto);   
+  async create(@Body() createCallbackDto: CreateCallbackDto) {
+    return await this.callbackService.create(createCallbackDto);
   }
 
-  @Get()
-  async findAll() {
-    return this.callbackService.findAll();
-  }
+  // @Get()
+  // async findAll() {
+  //   return this.callbackService.findAll();
+  // }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {   
-      return await this.callbackService.findById(id);
-  
+  async findOne(@Param('id') id: string) {
+    return await this.callbackService.findById(id);
   }
 
   @Put(':id/status')
   async updateStatus(
     @Param('id') id: string,
     @Body('isProcessed') isProcessed: boolean,
-  ) {    
-      return await this.callbackService.updateStatus(id, isProcessed);
- 
+  ) {
+    return await this.callbackService.updateStatus(id, isProcessed);
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {    
-      return await this.callbackService.delete(id);   
-    }
+  async remove(@Param('id') id: string) {
+    return await this.callbackService.delete(id);
   }
+  @Get()
+  async findAll(@Query() paginationQuery: PaginationQueryDto) {
+    return this.callbackService.findAll(paginationQuery);
+  }
+}
